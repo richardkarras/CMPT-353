ea8bc350912365e60932e1f67d7fa54ef08f36c1
Richard Karras <richard.karras@usask.ca>
2025-03-12 14:32:01 -0600

    a5 wip

---
+++ 2025/assignment5/app/nosqldb/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Things go here
+
+          <br></br>And this too?
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
